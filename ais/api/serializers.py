679e0f588b57d493b627fedc61ded40c98aaa5ce
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2016-04-15 11:19:35 -0400

    Order the address attributes when serializing

--- ais/api/serializers.py
+++ ais/api/serializers.py
@@ -1,22 +1,23 @@
 import json
+from collections import OrderedDict
 
 
 class AddressJsonSerializer:
     def model_to_data(self, address):
-        data = {
-            'street_address': address.street_address,
-            'address_low': address.address_low,
-            'address_low_suffix': address.address_low_suffix,
-            'address_low_frac': address.address_low_frac,
-            'address_high': address.address_high,
-            'street_predir': address.street_predir,
-            'street_name': address.street_name,
-            'street_suffix': address.street_suffix,
-            'street_postdir': address.street_postdir,
-            'unit_type': address.unit_type,
-            'unit_num': address.unit_num,
-            'street_full': address.street_full,
-        }
+        data = OrderedDict([
+            ('street_address', address.street_address),
+            ('address_low', address.address_low),
+            ('address_low_suffix', address.address_low_suffix),
+            ('address_low_frac', address.address_low_frac),
+            ('address_high', address.address_high),
+            ('street_predir', address.street_predir),
+            ('street_name', address.street_name),
+            ('street_suffix', address.street_suffix),
+            ('street_postdir', address.street_postdir),
+            ('unit_type', address.unit_type),
+            ('unit_num', address.unit_num),
+            ('street_full', address.street_full),
+        ])
         return data
 
     def serialize(self, address):
