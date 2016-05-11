699bd2b863b3a3d692f42997ff40cc2f718ffe0f
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2016-05-11 12:49:01 -0400

    Add a profiler, if PROFILE is True in config

--- run.py
+++ run.py
@@ -3,4 +3,7 @@ from ais import app
 # Importing ais.api will initialize the app's routes.
 import ais.api.views
 
+from werkzeug.contrib.profiler import ProfilerMiddleware
+app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[10])
+
 app.run(debug=app.config['DEBUG'])
