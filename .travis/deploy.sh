ce764e749de587a7e4ce58c39f602710747f921a
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2016-05-26 12:03:34 -0400

    Only install SSH keys if explicitly requested

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,10 +3,27 @@
 set -e
 
 SCRIPTS_DIR=$(dirname $0)
-KEYFILE=deploy.pem
+KEYFILE=${DEPLOY_KEYFILE:-"~/.ssh/deploy.pem"}
 INSTANCE_USER=ubuntu
 ENCRYPTION_KEY='encrypted_${ENCRYPTION_ID}_key'
 ENCRYPTION_IV='encrypted_${ENCRYPTION_ID}_iv'
+INSTALL_SSH=0
+
+for i in "$@" ; do
+  case $i in
+    -s|--install-ssh)
+      INSTALL_SSH=1
+      shift # past argument=value
+      ;;
+    -k=*|--keyfile=*)
+      KEYFILE="${i#*=}"
+      shift # past argument=value
+      ;;
+    *)
+            # unknown option
+    ;;
+  esac
+done
 
 
 # Install the AWS CLI if it's not already
@@ -20,17 +37,30 @@ INSTANCE_IP=`aws ec2 describe-instances --filters \
   "Name=tag:Project,Values=$PROJECT_NAME" | \
   grep '^INSTANCES' | cut -f14`
 if [ -z "$INSTANCE_IP" ]; then echo "No machine found for branch \"$TRAVIS_BRANCH\". Skipping deploy" && exit 0; fi
-ssh-keyscan -H $INSTANCE_IP | sudo tee --append /etc/ssh/ssh_known_hosts > /dev/null
 
+# *************************************
+# Set up SSH
+#
+# 1. Get the fingerprint of the EC2 instance and install it into the known
+#    hosts.
+# 2. Download and install a private key that the remote machine knows about.
+#
+# These steps will only run if you specify the -s|--install-ssh flag
 
-# Copy the SSH Key
-echo 'Decrypting and installing the SSH private key'
-aws s3 cp s3://phila-deploy/${PROJECT_NAME}/deploy.pem.enc.$TRAVIS_BRANCH deploy.pem.enc
-openssl aes-256-cbc -K \$${ENCRYPTION_KEY} -iv \$${ENCRYPTION_IV} -in deploy.pem.enc -out ~/.ssh/${KEYFILE} -d
-chmod 600 ~/.ssh/deploy.pem
-eval $(ssh-agent -s)
-ssh-add ~/.ssh/deploy.pem
+if [ $INSTALL_SSH == 1 ] ; then
+  echo 'Installing machine''s IP into known hosts'
+  ssh-keyscan -H $INSTANCE_IP | sudo tee --append /etc/ssh/ssh_known_hosts > /dev/null
 
+  # Copy the SSH Key
+  echo 'Decrypting and installing the SSH private key'
+  aws s3 cp s3://phila-deploy/${PROJECT_NAME}/deploy.pem.enc.$TRAVIS_BRANCH deploy.pem.enc
+  openssl aes-256-cbc -K \$${ENCRYPTION_KEY} -iv \$${ENCRYPTION_IV} -in deploy.pem.enc -out $KEYFILE -d
+  chmod 600 $KEYFILE
+  eval $(ssh-agent -s)
+  ssh-add $KEYFILE
+fi
+
+# *************************************
 
 # SSH onto the machine, install git if it's not already installed, and
 # download the latest version of the code. Set up the environment file and run
