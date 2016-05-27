f983dd85f34bc9ebb154cc368e60e22d7bdd4619
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2016-05-27 01:18:28 -0400

    Make sure the deploying user owns passyunk/pdata

--- .travis/update_zip4.sh
+++ .travis/update_zip4.sh
@@ -3,6 +3,6 @@
 echo 'Installing Zip4 data for address parsing'
 PASSYUNK_SCRIPT="import passyunk; from os.path import dirname; print(dirname(passyunk.__file__))"
 PASSYUNK_DIR=$(python3 -c "$PASSYUNK_SCRIPT")
-sudo chown `whoami`:`whoami` $PASSYUNK_DIR
-mkdir -p $PASSYUNK_DIR/pdata
+sudo mkdir -p $PASSYUNK_DIR/pdata
+sudo chown `whoami`:`whoami` $PASSYUNK_DIR/pdata
 aws s3 cp s3://ais-deploy/uspszip4.csv $PASSYUNK_DIR/pdata
