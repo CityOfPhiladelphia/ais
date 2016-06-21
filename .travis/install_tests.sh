a14995b60d026a2497d03fd6e109e3d3878ee37b
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2016-06-21 14:33:19 -0400

    Add PROJECT_NAME to test install script

--- .travis/install_tests.sh
+++ .travis/install_tests.sh
@@ -2,11 +2,13 @@
 
 set -ex
 
+PROJECT_NAME=$(python -c "print('$TRAVIS_REPO_SLUG'.split('/')[1])")
+
 # Set up the virtual environment
 .travis/init_environment.sh
 
 # Set up AWS access
-.travis/init_awscli.sh '$AWS_ID' '$AWS_SECRET'
+.travis/init_awscli.sh $AWS_ID $AWS_SECRET
 
 # Transfer the .env file to the server
 .travis/init_envfile.sh $PROJECT_NAME $TRAVIS_BRANCH
