9c9810b484d12ba8c750bd4e296fa7f6e19fd5d2
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2016-06-21 16:53:47 -0400

    Always configure the AWS credentials

--- .travis/init_awscli.sh
+++ .travis/init_awscli.sh
@@ -5,18 +5,18 @@ set -e
 AWS_ID=$1
 AWS_SECRET=$2
 
-if test ! -f ~/.aws/config ; then
-    source env/bin/activate
+source env/bin/activate
+if ! aws help &>/dev/null ; then
     pip install awscli
-    deactivate
+fi
 
-    echo 'Configuring AWS CLI'
-    mkdir -p ~/.aws
-    cat > ~/.aws/config <<EOF
+echo 'Configuring AWS CLI'
+mkdir -p ~/.aws
+cat > ~/.aws/config <<EOF
 [default]
 aws_access_key_id = $AWS_ID
 aws_secret_access_key = $AWS_SECRET
 output = text
 region = us-east-1
 EOF
-fi
+deactivate
