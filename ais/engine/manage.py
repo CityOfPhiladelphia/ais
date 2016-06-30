5c17ba250ae5901424439a0171c4d6d0293db062
Robert Martin <rmartin@rmart.in>
2016-06-30 11:02:18 -0400

    Update another deprecated Flask extension import

--- ais/engine/manage.py
+++ ais/engine/manage.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import subprocess
-from flask.ext.script import Manager
+from flask_script import Manager
 from ais import app
 
 manager = Manager(usage='Perform engine operations')
@@ -9,7 +9,9 @@ manager = Manager(usage='Perform engine operations')
 def path_for_script(root_path, script):
     return os.path.join(root_path, 'engine', 'scripts', script + '.py')
 
-@manager.option('script', help='Name of the script to run, e.g. `load_addresses`. Use `all` to run all scripts.')
+@manager.option('script', help='Name of the script to run, e.g. '
+                               '`load_addresses`. Use `all` to run all scripts.'
+)
 def run(script):
     """Run engine scripts."""
     root_path = app.root_path
