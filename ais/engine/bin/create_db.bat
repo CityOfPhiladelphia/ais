dbf577be026ca7aa3b109cf5c496cc4dbb0ce1cc
andy.rothwell <rothwell.andy@gmail.com>
2016-08-17 14:44:58 -0400

    fix db_create script

--- ais/engine/bin/create_db.bat
+++ ais/engine/bin/create_db.bat
@@ -10,15 +10,15 @@ set PGPASSWORD=%POSTGRES_PASSWORD%
 
 rem create engine user and start using it
 psql -c "create role %ENGINE_USER% with superuser login password '%ENGINE_PASSWORD%';"
-set PGUSER=%ENGINE_USER%
-set PGPASSWORD=%ENGINE_PASSWORD%
 
 rem create database
-psql -c "create database %ENGINE_DB%;"
+psql -c "create database %ENGINE_DB% with owner %ENGINE_USER%;"
 
 rem create extensions
+set PGUSER=%ENGINE_USER%
+set PGPASSWORD=%ENGINE_PASSWORD%
 psql -c "create extension postgis;create extension adminpack;create extension pg_trgm;"
 
 rem allow internal connections to the db
-echo host    ais_engine       ais_engine     0.0.0.0/0               md5 >> "%PGDATA%\pg_hba.conf"
+echo host    ais_engine       ais_engine     0.0.0.0/0               md5 >> %PGDATA%\pg_hba.conf
 pg_ctl reload
