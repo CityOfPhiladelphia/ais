dcaf38b2a883625108533ede032bf49ea9f33100
Robert Martin <rmartin@rmart.in>
2016-09-08 12:29:21 -0400

    Revert part of 2d1db0d

--- ais/engine/scripts/load_addresses.py
+++ ais/engine/scripts/load_addresses.py
@@ -534,8 +534,7 @@ for address in addresses:
             # If there were warnings, raise them again
             for warning in street_warning_map.get(base_address, []):
                 had_street_warning(
-                    #warning['street_address'],
-                    base_address,
+                    warning['street_address'],
                     warning['reason'],
                     notes=warning.get('notes')
                 )
