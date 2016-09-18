2f1aed93296a8095c3e01e03b0911ae506282a17
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2016-09-17 21:16:18 -0400

    Use the flag for naming the environment when settings blug/green status

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -42,8 +42,8 @@ if [ "$EB_BLUEGREEN_STATUS" = "Swap" ] ; then
 
   echo "Setting deployment environment variables"
   # Start as background processes in parallel
-  eb setenv $EB_OLD_PROD_ENV EB_BLUEGREEN_STATUS=Staging &
-  eb setenv $EB_NEW_PROD_ENV EB_BLUEGREEN_STATUS=Production &
+  eb setenv --environment $EB_OLD_PROD_ENV EB_BLUEGREEN_STATUS=Staging &
+  eb setenv --environment $EB_NEW_PROD_ENV EB_BLUEGREEN_STATUS=Production &
   # Wait for both
   wait $(jobs -p)
 
