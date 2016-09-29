47bdf22dce7c0bab07a75dd5cc60eba378f6aa7d
CITY\Alex.waldman <alex.waldman@phila.gov>
2016-09-29 10:54:04 -0400

    edit ps1 script so backward compatible

--- ais/engine/bin/update_db.ps1
+++ ais/engine/bin/update_db.ps1
@@ -8,28 +8,31 @@ echo "Activating virtual environment"
 cd ../../../env/scripts
 ./activate
 
-echo "Running the engine"
+#echo "Running the engine"
 
-ais engine run load_streets
-ais engine run load_street_aliases
-ais engine run load_opa_properties
-ais engine run load_dor_parcels
-ais engine run load_pwd_parcels
-ais engine run load_curbs
-ais engine run load_addresses
-ais engine run load_zip_ranges
-ais engine run geocode_addresses
-ais engine run make_address_summary
-ais engine run load_service_areas
-ais engine run make_service_area_summary
+#ais engine run load_streets
+#ais engine run load_street_aliases
+#ais engine run load_opa_properties
+#ais engine run load_dor_parcels
+#ais engine run load_pwd_parcels
+#ais engine run load_curbs
+#ais engine run load_addresses
+#ais engine run load_zip_ranges
+#ais engine run geocode_addresses
+#ais engine run make_address_summary
+#ais engine run load_service_areas
+#ais engine run make_service_area_summary
 
+echo "Running engine tests"
+#pytest ../../ais/engine
+
+# Dumping the engine
 $db_dump_file_loc = $env:temp + "\ais_engine.dump"
-
-# Run pg_dump
 echo "Dumping the engine DB"
-pg_dump -Fc -U ais_engine -n public ais_engine > db_dump_file_loc
+$db_dump_file_loc
+pg_dump -Fc -U ais_engine -n public ais_engine > $db_dump_file_loc
 
-# Get Staging Environment
+# Getting Staging Environment
 
 # Find the environment that is either marked as staging or ready to swap in.
 
@@ -62,7 +65,9 @@ echo $ENV_STATUS_NAME;
 echo "Obtaining the staging/swap database uri"
 $vars = eb printenv $ENV_VAR_NAME
 $b=$vars -like '*SQLALCHEMY_DATABASE_URI*'
-$db_uri=$b.split('@')[1].split(':')[0]
+# $db_uri=$b.split('@')[1].split(':')[0] # this is powershell > 2.0 syntax (5.0)
+$c=($b -split '@')[1]
+$db_uri=($c -split ':')[0]
 $db_uri
 
 # NOTE: SPIN UP STAGING/SWAP INSTANCE NOW.
@@ -85,7 +90,8 @@ echo "Restarting the latest master branch build (requires travis CLI)"
 
 # Get last travis build and parse build number
 $lb = travis history --branch master --limit 1
-$LAST_BUILD = $l.split(' ')[0].split('#')[1]
+#$LAST_BUILD = $lb.split(' ')[0].split('#')[1] # this is powershell > 2.0 syntax (5.0)
+$LAST_BUILD = ((($lb -split(' '))[0]) -split('#'))[1]
 travis restart $LAST_BUILD
 
 # NOTE: Travis-CI will take over from here. Check in the .travis/deploy script
