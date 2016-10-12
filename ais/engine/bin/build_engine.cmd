f330ae757734e13685cfae8d48de09f942b01018
CITY\Alex.waldman <alex.waldman@phila.gov>
2016-10-12 09:42:04 -0400

    remove rems in build script

--- ais/engine/bin/build_engine.cmd
+++ ais/engine/bin/build_engine.cmd
@@ -14,40 +14,40 @@ call activate.bat
 echo. && echo "Running the engine"
 
 echo. && echo "Loading Streets"
-REM ais engine run load_streets
+ais engine run load_streets
 
 echo. && echo "Loading Street Aliases"
-REM ais engine run load_street_aliases
+ais engine run load_street_aliases
 
 echo. && echo "Loading OPA Properties"
-REM ais engine run load_opa_properties
+ais engine run load_opa_properties
 
 echo. && echo "Loading DOR parcels"
-REM ais engine run load_dor_parcels
+ais engine run load_dor_parcels
 
 echo. && echo "Loading PWD Parcels"
-REM ais engine run load_pwd_parcels
+ais engine run load_pwd_parcels
 
 echo. && echo "Loading Curbs"
-REM ais engine run load_curbs
+ais engine run load_curbs
 
 echo. && echo "Loading Addresses"
-REM ais engine run load_addresses
+ais engine run load_addresses
 
 echo. && echo "Loading Zip Ranges"
-REM ais engine run load_zip_ranges
+ais engine run load_zip_ranges
 
 echo. && echo "Geocoding Addresses"
-REM ais engine run geocode_addresses
+ais engine run geocode_addresses
 
 echo. && echo "Making Address Summary"
-REM ais engine run make_address_summary
+ais engine run make_address_summary
 
 echo. && echo "Loading Service Areas"
-REM ais engine run load_service_areas
+ais engine run load_service_areas
 
 echo. && echo "Making Service Area Summary"
-REM ais engine run make_service_area_summary
+ais engine run make_service_area_summary
 
 
 rem Get end time:
