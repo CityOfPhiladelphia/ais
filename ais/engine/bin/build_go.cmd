a6c15a442dbd57cf1b164229737630a50c78eb7a
CITY\Alex.waldman <alex.waldman@phila.gov>
2016-11-03 11:28:54 -0400

    Change date format of build log file names

--- ais/engine/bin/build_go.cmd
+++ ais/engine/bin/build_go.cmd
@@ -1,1 +1,1 @@
-CALL build_engine > build_log_%date:~-4,4%%date:~-7,2%%date:~-10,2%.txt 2> build_errors_%date:~-4,4%%date:~-7,2%%date:~-10,2%.txt
+CALL build_engine > build_log_%date:~-4,4%%date:~-10,2%%date:~-7,2%.txt 2> build_errors_%date:~-4,4%%date:~-10,2%%date:~-7,2%.txt
