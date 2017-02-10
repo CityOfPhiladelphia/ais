b7dde74639810febfc633503a7663a37885daa4c
CITY\Alex.waldman <alex.waldman@phila.gov>
2017-02-10 10:23:26 -0500

    Update voter source table in config.

--- config.py
+++ config.py
@@ -299,7 +299,7 @@ ADDRESSES = {
         # },
         {
             'name':                 'voters',
-            'table':                'gis_elections.voters_2017_01',
+            'table':                'gis_elections.voters_2017_02',
             'db':                   'gis',
             'address_fields':       {
                 'address_low':          'house__',
