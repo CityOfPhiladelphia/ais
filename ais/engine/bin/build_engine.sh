04794df3be244b0c358eb6c877839d8d318fadc4
CITY\Alex.waldman <alex.waldman@phila.gov>
2017-03-01 11:45:50 -0500

    Add new scripts to engine build routine.

--- ais/engine/bin/build_engine.sh
+++ ais/engine/bin/build_engine.sh
@@ -39,6 +39,9 @@ ais engine run make_linked_tags
 echo "Geocoding Addresses"
 ais engine run geocode_addresses
 
+echo "Geocoding Addresses from Links"
+ais engine run geocode_addresses_from_links
+
 echo "Making Address Summary"
 ais engine run make_address_summary
 
