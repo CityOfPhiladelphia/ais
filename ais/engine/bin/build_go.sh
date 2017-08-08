eff69667368415d0ac695c7a27a3de33947b034e
CITY\Alex.waldman <alex.waldman@phila.gov>
2017-08-08 12:37:13 -0400

    Fix date suffix for logs.

--- ais/engine/bin/build_go.sh
+++ ais/engine/bin/build_go.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 
+datestamp=$(date +%Y%m%d)
 start_dt=$(date +%Y%m%d%T)
 echo "Started: "$start_dt
 
 error_file="build_errors_"
 out_file="build_log_"
-error_file_loc=../log/$error_file$dt.txt
-out_file_loc=../log/$out_file$dt.txt
+error_file_loc=../log/$error_file$datestamp.txt
+out_file_loc=../log/$out_file$datestamp.txt
 
 mkdir -p ../log
 
-(bash build_engine.sh 1>&2) 2> >(tee $out_file_loc) > >(tee $error_file_loc)
+bash build_engine.sh > >(tee -a $out_file_loc) 2> >(tee -a $error_file_loc >&2)
 
 end_dt=$(date +%Y%m%d%T)
 
 echo "Time Summary: "
 echo "Started: "$start_dt
 echo "Finished: "$end_dt
+
+
+
+
+
+
