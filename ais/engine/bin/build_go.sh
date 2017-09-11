94d30b4dae4b71d02e6831f4fc54a04afc54cfa7
CITY\Alex.waldman <alex.waldman@phila.gov>
2017-09-11 17:20:06 -0400

    Update db and restart last Travis build.

--- ais/engine/bin/build_go.sh
+++ ais/engine/bin/build_go.sh
@@ -35,7 +35,7 @@ echo "Production environment is: "$eb_prod_env
 echo "Running engine tests."
 error_file_loc=../log/pytest_engine_errors_$datestamp.txt
 out_file_loc=../log/pytest_engine_log_$datestamp.txt
-#pytest ../tests/test_engine.py > >(tee -a $out_file_loc) 2> >(tee -a $error_file_loc >&2)
+pytest ../tests/test_engine.py > >(tee -a $out_file_loc) 2> >(tee -a $error_file_loc >&2)
 if [ $? -ne 0 ]
 then
   echo "Engine tests failed"
@@ -45,20 +45,25 @@ fi
 echo "Running API tests."
 error_file_loc=../log/pytest_api_errors_$datestamp.txt
 out_file_loc=../log/pytest_api_log_$datestamp.txt
-#pytest ../../api/tests/  > >(tee -a $out_file_loc) 2> >(tee -a $error_file_loc >&2)
+pytest ../../api/tests/  > >(tee -a $out_file_loc) 2> >(tee -a $error_file_loc >&2)
 if [ $? -ne 0 ]
 then
   echo "API tests failed"
   exit 1;
 fi
 
+# Update (Restore) AWS RDS instance
+
 # Make a copy (Dump) the newly built local engine db
 echo "Copying the engine database."
-#mkdir -p ../backup
-#db_dump_file_loc=../backup/ais_engine.dump
-#pg_dump -Fc -U ais_engine -n public ais_engine > $db_dump_file_loc
-
-# Update (Restore) AWS RDS instance
+mkdir -p ../backup
+db_dump_file_loc=../backup/ais_engine.dump
+pg_dump -Fc -U ais_engine -n public ais_engine > $db_dump_file_loc
+if [ $? -ne 0 ]
+then
+  echo "DB dump failed"
+  exit 1;
+fi
 
 # Get AWS staging environment
 echo "Finding the staging environment"
@@ -73,4 +78,38 @@ db_uri=$(get_db_uri $eb_staging_env || {
   echo "Could not find the db uri" ;
   exit 1 ;
 })  
-echo $db_uri
+echo "Staging database uri: "$db_uri
+
+echo "Restoring the engine DB into the $eb_staging_env environment "
+
+psql -U ais_engine -h $db_uri -d ais_engine -c "DROP SCHEMA IF EXISTS public CASCADE;"
+psql -U ais_engine -h $db_uri -d ais_engine -c "CREATE SCHEMA public;"
+psql -U ais_engine -h $db_uri -d ais_engine -c "GRANT ALL ON SCHEMA public TO postgres;"
+psql -U ais_engine -h $db_uri -d ais_engine -c "GRANT ALL ON SCHEMA public TO public;"
+psql -U ais_engine -h $db_uri -d ais_engine -c "CREATE EXTENSION postgis;"
+psql -U ais_engine -h $db_uri -d ais_engine -c "CREATE EXTENSION pg_trgm;"
+pg_restore -h $db_uri -d ais_engine -U ais_engine -c $db_dump_file_loc
+
+if [ $? -ne 0 ]
+then
+  echo "DB restore failed"
+  exit 1;
+fi
+
+echo "Marking the $eb_staging_env environment as ready for testing (swap)"
+eb setenv -e $eb_staging_env EB_BLUEGREEN_STATUS=Swap
+
+echo "Restarting the latest master branch build (requires travis CLI)"
+if ! hash travis ; then
+  echo "This step requires the Travis-CI CLI. To install and configure, see:
+  https://github.com/travis-ci/travis.rb#installation"
+  exit 1
+fi
+LAST_BUILD=$(travis history --branch master --limit 1 | cut --fields=1 --delimiter=" ")
+# The build number has a number sign as the first character. We need to strip
+# it off.
+LAST_BUILD=${LAST_BUILD:1}
+travis restart $LAST_BUILD
+
+# NOTE: Travis-CI will take over from here. Check in the .travis/deploy script
+# for further step.
