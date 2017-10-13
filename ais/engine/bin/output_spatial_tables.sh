f54bc354417ddb29473a02ea1e72a63af1dc7ae2
Alexander-M-Waldman <alex.waldman@phila.gov>
2017-10-13 17:59:14 -0400

    Update connection strings.

--- ais/engine/bin/output_spatial_tables.sh
+++ ais/engine/bin/output_spatial_tables.sh
@@ -12,8 +12,8 @@ python remove_special_characters.py "address_summary.csv" "address_summary_clean
 the_el describe_table address_summary --connection-string $postgis_dsn --output-file address_summary_schema.json --geometry-support postgis
 ###the_el create_table temp_address_summary --connection-string $oracle_dsn_gis_ais address_summary_schema.json --geometry-support sde --db-schema GIS_AIS
 python create_table_from_schema.py "address_summary"
-the_el write temp_address_summary --connection-string $oracle_dsn_gis_ais --table-schema-path address_summary_schema.json --geometry-support sde --db-schema GIS_AIS --input-file address_summary_cleaned.csv --skip-headers
-the_el swap_table temp_address_summary address_summary --connection-string $oracle_dsn_gis_ais #--select-users 'SDE, GIS_OPA, GIS_SDE_VIEWER' # Resolve grant select on address_sukmary to gis_opa with grant option' requirement either on gis_oddt.vw_property_summary end or incorporate into the-el swap_table oracle section
+the_el write t_address_summary --connection-string $oracle_dsn_gis_ais --table-schema-path address_summary_schema.json --geometry-support sde --input-file address_summary_cleaned.csv --skip-headers
+the_el swap_table t_address_summary address_summary --connection-string $oracle_dsn_gis_ais
 psql -U ais_engine -h localhost -d ais_engine -c "ALTER TABLE address_summary DROP COLUMN objectid;"
 psql -U ais_engine -h localhost -d ais_engine -c "ALTER TABLE address_summary DROP COLUMN shape;"
 
@@ -22,5 +22,5 @@ the_el read dor_parcel_address_analysis --connection-string $postgis_dsn --outpu
 the_el describe_table dor_parcel_address_analysis --connection-string $postgis_dsn --output-file dor_parcel_address_analysis_schema.json --geometry-support postgis
 #the_el create_table TEMP_DOR_PARCEL_ADDRESS_COMP_ANALYSIS --connection-string $oracle_dsn_gis_dor dor_parcel_address_comp_analysis_schema.json --geometry-support sde --db-schema GIS_DOR
 python create_table_from_schema.py "dor_parcel_address_analysis"
-the_el write temp_dor_parcel_address_analysis --connection-string $oracle_dsn_gis_ais --table-schema-path dor_parcel_address_analysis_schema.json --geometry-support sde-char --db-schema GIS_DOR --input-file dor_parcel_address_analysis.csv --skip-headers
-the_el swap_table temp_dor_parcel_address_analysis dor_parcel_address_analysis --connection-string $oracle_dsn_gis_ais
+the_el write t_dor_parcel_address_analysis --connection-string $oracle_dsn_gis_ais --table-schema-path dor_parcel_address_analysis_schema.json --geometry-support sde --input-file dor_parcel_address_analysis.csv --skip-headers
+the_el swap_table t_dor_parcel_address_analysis dor_parcel_address_analysis --connection-string $oracle_dsn_gis_ais
