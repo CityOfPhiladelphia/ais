71a8d646a6630ebda0033295ccff192196dacd98
Alexander-M-Waldman <alex.waldman@phila.gov>
2017-10-21 18:04:12 -0400

    Fix python script call.

--- ais/engine/bin/output_spatial_tables.sh
+++ ais/engine/bin/output_spatial_tables.sh
@@ -19,7 +19,7 @@ echo "cleaning address_summary"
 python remove_special_characters.py "address_summary.csv" "address_summary_cleaned.csv"
 echo "describing address_summary"
 the_el describe_table address_summary_transformed --connection-string $postgis_dsn --output-file address_summary_schema.json --geometry-support postgis
-python remove_schema_constraints "address_summary_schema.json"
+python remove_schema_constraints.py "address_summary_schema.json"
 echo "creating temp table"
 python create_table_from_schema.py "address_summary"
 echo "writing to temp table"
@@ -48,24 +48,24 @@ psql -U ais_engine -h localhost -d ais_engine -c "ALTER TABLE address_summary_tr
 psql -U ais_engine -h localhost -d ais_engine -c "ALTER TABLE address_summary_transformed DROP COLUMN shape;"
 
 #DOR_PARCEL_ADDRESS_COMP_ANALYSIS
-echo "Updating dor_parcel_address_analysis"
-the_el read dor_parcel_address_analysis --connection-string $postgis_dsn --output-file dor_parcel_address_analysis.csv --geometry-support postgis --from-srid 2272 --to-srid 2272
-the_el describe_table dor_parcel_address_analysis --connection-string $postgis_dsn --output-file dor_parcel_address_analysis_schema.json --geometry-support postgis
-python create_table_from_schema.py "dor_parcel_address_analysis"
-the_el write t_dor_parcel_address_analysis --connection-string $oracle_dsn_gis_ais --table-schema-path dor_parcel_address_analysis_schema.json --geometry-support sde --input-file dor_parcel_address_analysis.csv --skip-headers
-if [ $? -ne 0 ]
-then
-  echo "Writing to temp dor_parcel_address_analysis table failed. Exiting."
-  send_slack "Writing to dor_parcel_address_analysis table failed. Exiting."
-  exit 1;
-fi
-the_el swap_table t_dor_parcel_address_analysis dor_parcel_address_analysis --connection-string $oracle_dsn_gis_ais
-if [ $? -ne 0 ]
-then
-  echo "dor_parcel_address_analysis table swap failed. Exiting"
-  send_slack "dor_parcel_address_analysis table swap failed. Exiting."
-  exit 1;
-fi
-echo "Completed updating dor_parcel_address_analysis in DataBridge."
-send_slack "Completed updating dor_parcel_address_analysis in DataBridge."
+#echo "Updating dor_parcel_address_analysis"
+#the_el read dor_parcel_address_analysis --connection-string $postgis_dsn --output-file dor_parcel_address_analysis.csv --geometry-support postgis --from-srid 2272 --to-srid 2272
+#the_el describe_table dor_parcel_address_analysis --connection-string $postgis_dsn --output-file dor_parcel_address_analysis_schema.json --geometry-support postgis
+#python create_table_from_schema.py "dor_parcel_address_analysis"
+#the_el write t_dor_parcel_address_analysis --connection-string $oracle_dsn_gis_ais --table-schema-path dor_parcel_address_analysis_schema.json --geometry-support sde --input-file dor_parcel_address_analysis.csv --skip-headers
+#if [ $? -ne 0 ]
+#then
+#  echo "Writing to temp dor_parcel_address_analysis table failed. Exiting."
+#  send_slack "Writing to dor_parcel_address_analysis table failed. Exiting."
+#  exit 1;
+#fi
+#the_el swap_table t_dor_parcel_address_analysis dor_parcel_address_analysis --connection-string $oracle_dsn_gis_ais
+#if [ $? -ne 0 ]
+#then
+#  echo "dor_parcel_address_analysis table swap failed. Exiting"
+#  send_slack "dor_parcel_address_analysis table swap failed. Exiting."
+#  exit 1;
+#fi
+#echo "Completed updating dor_parcel_address_analysis in DataBridge."
+#send_slack "Completed updating dor_parcel_address_analysis in DataBridge."
 
