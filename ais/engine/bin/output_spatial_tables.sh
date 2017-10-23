ff6cc5bf76dd41f7bfd5eee5b70e703d1f9cf684
Alexander-M-Waldman <alex.waldman@phila.gov>
2017-10-23 15:25:32 -0400

    Pass from-srid param in the-el write command.

--- ais/engine/bin/output_spatial_tables.sh
+++ ais/engine/bin/output_spatial_tables.sh
@@ -14,7 +14,7 @@ ALTER TABLE address_summary_transformed ADD COLUMN shape geometry(Point,2272);
 UPDATE address_summary_transformed SET shape = st_setsrid(st_makepoint(geocode_x, geocode_y), 2272);
 EOF
 echo "reading address_summary"
-the_el read address_summary_transformed --connection-string $postgis_dsn --output-file address_summary.csv --geometry-support postgis --from-srid 2272 --to-srid 2272
+the_el read address_summary_transformed --connection-string $postgis_dsn --output-file address_summary.csv --geometry-support postgis
 echo "cleaning address_summary"
 python remove_special_characters.py "address_summary.csv" "address_summary_cleaned.csv"
 echo "describing address_summary"
@@ -23,7 +23,7 @@ python remove_schema_constraints.py "address_summary_schema.json"
 echo "creating temp table"
 python create_table_from_schema.py "address_summary"
 echo "writing to temp table"
-the_el write t_address_summary --connection-string $oracle_dsn_gis_ais --table-schema-path address_summary_schema.json --geometry-support sde --input-file address_summary_cleaned.csv --skip-headers
+the_el write t_address_summary --connection-string $oracle_dsn_gis_ais --table-schema-path address_summary_schema.json --geometry-support sde --from-srid 2272 --input-file address_summary_cleaned.csv --skip-headers
 if [ $? -ne 0 ]
 then
   echo "Writing to temp address_summary table failed. Exiting."
@@ -52,7 +52,7 @@ psql -U ais_engine -h localhost -d ais_engine -c "ALTER TABLE address_summary_tr
 #the_el read dor_parcel_address_analysis --connection-string $postgis_dsn --output-file dor_parcel_address_analysis.csv --geometry-support postgis --from-srid 2272 --to-srid 2272
 #the_el describe_table dor_parcel_address_analysis --connection-string $postgis_dsn --output-file dor_parcel_address_analysis_schema.json --geometry-support postgis
 #python create_table_from_schema.py "dor_parcel_address_analysis"
-#the_el write t_dor_parcel_address_analysis --connection-string $oracle_dsn_gis_ais --table-schema-path dor_parcel_address_analysis_schema.json --geometry-support sde --input-file dor_parcel_address_analysis.csv --skip-headers
+#the_el write t_dor_parcel_address_analysis --connection-string $oracle_dsn_gis_ais --table-schema-path dor_parcel_address_analysis_schema.json --geometry-support sde --from-srid 2272 --input-file dor_parcel_address_analysis.csv --skip-headers
 #if [ $? -ne 0 ]
 #then
 #  echo "Writing to temp dor_parcel_address_analysis table failed. Exiting."
