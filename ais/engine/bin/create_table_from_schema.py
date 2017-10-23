72495738e3472221781e4b414f343c153560068d
Alexander-M-Waldman <alex.waldman@phila.gov>
2017-10-23 15:33:04 -0400

    Use shorter table name.

--- ais/engine/bin/create_table_from_schema.py
+++ ais/engine/bin/create_table_from_schema.py
@@ -5,7 +5,7 @@ from ais import app
 table_name = sys.argv[1]
 dsn_map = {
     'address_summary': 'GIS_AIS_CONN',
-    'dor_parcel_address_analysis': 'GIS_AIS_CONN'
+    'dor_parcel_address_check': 'GIS_AIS_CONN'
 }
 config = app.config
 dsn = config[dsn_map[table_name]]
@@ -18,7 +18,7 @@ sql3 = "GRANT SELECT ON t_{} to GIS_SDE_VIEWER".format(table_name)
 if table_name == 'address_summary':
     sql1 = "CREATE TABLE t_{table_name} as (select * from test_{table_name} where 1=0)".format(table_name=table_name)
     sql4 = "GRANT SELECT on t_{} to GIS_OPA with GRANT OPTION".format(table_name)
-elif table_name == 'dor_parcel_address_analysis':
+elif table_name == 'dor_parcel_address_check':
     sql4 = "GRANT SELECT ON t_{} to GIS_DOR".format(table_name)
 curs.execute(sql1)
 curs.execute(sql2)
