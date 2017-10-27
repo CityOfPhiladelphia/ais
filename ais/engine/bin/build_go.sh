19e255eff38f836e61225e02ff668eb17a96e27d
Alexander-M-Waldman <alex.waldman@phila.gov>
2017-10-27 18:04:18 -0400

    Call reports_go.sh independently from engine build.

--- ais/engine/bin/build_go.sh
+++ ais/engine/bin/build_go.sh
@@ -146,16 +146,17 @@ then
 fi
 send_slack "New AIS build has been deployed."
 
-echo "Making engine reports."
-send_slack "Starting reporting."
-error_file_loc=../log/reports_errors_$datestamp.txt
-out_file_loc=../log/reports_log_$datestamp.txt
-python make_reports.py  > >(tee -a $out_file_loc) 2> >(tee -a $error_file_loc >&2)
-if [ $? -ne 0 ]
-then
-  echo "Reports failed"
-  send_slack "Engine reports did not complete."
-  exit 1;
-fi
-send_slack "Engine reports have completed."
+# Reporting is called independently
+#echo "Making engine reports."
+#send_slack "Starting reporting."
+#error_file_loc=../log/reports_errors_$datestamp.txt
+#out_file_loc=../log/reports_log_$datestamp.txt
+#python make_reports.py  > >(tee -a $out_file_loc) 2> >(tee -a $error_file_loc >&2)
+#if [ $? -ne 0 ]
+#then
+#  echo "Reports failed"
+#  send_slack "Engine reports did not complete."
+#  exit 1;
+#fi
+#send_slack "Engine reports have completed."
 
--- ais/engine/bin/reports_go.sh
+++ ais/engine/bin/reports_go.sh
@@ -9,16 +9,18 @@ source ../../../env/bin/activate
 source ../../../bin/eb_env_utils.sh
 
 echo "Starting reporting."
+send_slack "Starting reporting."
+
 python make_reports.py
 if [ $? -ne 0 ]
 then
   echo "Reporting has failed"
-  send_slack "Reporting has failed."
+  send_slack "Engine reports did not complete."
   exit 1;
 fi
-send_slack "Reporting has completed."
-
+send_slack "Engine reports have completed."
 
 end_dt=$(date +%Y%m%d%T)
 echo finished at $end_dt
+
 
