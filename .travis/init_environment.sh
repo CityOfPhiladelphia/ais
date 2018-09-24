5dbfc6025577e2aa09bc03653dfa486fbe21a26f
CITY\Alex.waldman <alex.waldman@phila.gov>
2018-09-24 12:57:25 -0400

    Debug travis issue - output Travis IP

--- .travis/init_environment.sh
+++ .travis/init_environment.sh
@@ -8,7 +8,11 @@ if ! python3.5 -V &>/dev/null ; then
   sudo add-apt-repository ppa:fkrull/deadsnakes
   sudo apt-get update
   sudo apt-get install -y python3.5 python3.5-dev
+  sudo apt-get install -y iproute2
 fi
 
 echo 'Create a virutal environment'
 virtualenv env -p python3.5 || echo 'Virtual environment already created.'
+
+echo 'Writing Travis instance IP info:'
+ip a
