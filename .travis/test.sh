fcb42e4bd0817ebdb85c90e5cd2b9bbf72eeb2c2
CITY\Alex.waldman <alex.waldman@phila.gov>
2018-10-22 17:39:05 -0400

    Install nmap for test

--- .travis/test.sh
+++ .travis/test.sh
@@ -4,5 +4,6 @@ set -e
 
 source env/bin/activate
 pip install pytest honcho --force-reinstall
+sudo apt install nmap
 nmap -p 5432 $rds_market_dsn
 honcho run pytest ais -s --ignore=ais/engine/tests
