85ba9c622d1a032b06b3f1b6a803c898e64f309a
CITY\Alex.waldman <alex.waldman@phila.gov>
2018-10-23 16:24:18 -0400

    test

--- .travis/test.sh
+++ .travis/test.sh
@@ -6,6 +6,6 @@ source env/bin/activate
 pip install pytest honcho --force-reinstall
 sudo apt install nmap
 nmap -p 5432 $rds_market_dsn -Pn
-telnet $rds_market_dsn 5432
+#telnet $rds_market_dsn 5432
 PGPASSWORD=$pgpassword psql -U ais_engine -d ais_engine -h $rds_market_dsn -c "select count(*) from address_summary;"
 honcho run pytest ais -s --ignore=ais/engine/tests
