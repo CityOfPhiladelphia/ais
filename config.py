f4cf76cfcbd9ecdcf17401d0252bf1da93cc2cce
CITY\Alex.waldman <alex.waldman@phila.gov>
2019-02-05 14:14:58 -0500

    Read from view

--- config.py
+++ config.py
@@ -409,7 +409,7 @@ ADDRESSES = {
         },
         {
             'name': 'building_footprints',
-            'table': 'gis_ais_sources.li_building_footprints_rev_fmt',
+            'table': 'gis_ais_sources.vw_li_bldg_ftprnts_rev_fmt',
             'db':    'gis',
             'address_fields': {
                 'street_address': 'address',
