7933fc123559bf9388c5d3916758431f572e38e6
CITY\Alex.waldman <alex.waldman@phila.gov>
2019-05-31 13:29:11 -0400

    Update building_footprints source

--- config.py
+++ config.py
@@ -409,7 +409,7 @@ ADDRESSES = {
         },
         {
             'name': 'building_footprints',
-            'table': 'gis_lni.li_building_footprints_rev',
+            'table': 'gis_lni.li_building_footprints',
             'db':    'gis',
             'address_fields': {
                 'street_address': 'address',
