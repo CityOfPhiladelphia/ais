6ef037c87bf181caab98b3bbc16099ff421bf914
Alexander-M-Waldman <alex.waldman@phila.gov>
2020-10-09 09:36:06 -0400

    Update apt package repo

--- .travis/init_environment.sh
+++ .travis/init_environment.sh
@@ -5,7 +5,8 @@ set -e
 # Make sure Python 3.6 is installed
 if ! python3.6 -V &>/dev/null ; then
   echo 'Install Python 3.6'
-  sudo add-apt-repository ppa:fkrull/deadsnakes
+#  sudo add-apt-repository ppa:fkrull/deadsnakes
+  sudo add-apt-repository ppa:deadsnakes/ppa
   sudo apt-get update
   sudo apt-get install -y python3.6 python3.6-dev
   sudo apt-get install -y iproute2
