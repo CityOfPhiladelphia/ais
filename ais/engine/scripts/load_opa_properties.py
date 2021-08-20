bac12da530a0e9879744e8c8a75ee178734f00f5
Alexander-M-Waldman <alex.waldman@phila.gov>
2021-08-20 17:44:33 -0400

    Update load_opa_properties and config to read from new OPA sources

--- ais/engine/scripts/load_opa_properties.py
+++ ais/engine/scripts/load_opa_properties.py
@@ -15,7 +15,6 @@ start = datetime.now()
 """SET UP"""
 
 config = app.config
-opa_owners_source_table = config['BASE_DATA_SOURCES']['opa_owners']['table']
 source_def = config['BASE_DATA_SOURCES']['properties']
 source_db = datum.connect(config['DATABASES'][source_def['db']])
 ais_source_db = datum.connect(config['DATABASES']['gis'])
@@ -34,8 +33,6 @@ source_fields = list(field_map.values())
 source_tencode_field = field_map['tencode']
 source_account_num_field = field_map['account_num']
 source_address_field = field_map['source_address']
-source_address_suffix_field = field_map['address_suffix']
-source_unit_field = field_map['unit']
 
 print('Dropping index...')
 prop_table.drop_index('street_address')
@@ -45,12 +42,8 @@ prop_table.delete()
 
 print('Reading owners from source...')
 owner_stmt = """
-	select
-		account_num,
-		listagg(trim(name), '|') within group(order by account_num) as owners
-	from {source_owners_table}
-	group by account_num
-""".format(source_owners_table=opa_owners_source_table)
+select * from vw_opa_owners_cama
+"""
 owner_rows = ais_source_db.execute(owner_stmt)
 owner_map = {x[0]: x[1] for x in owner_rows}
 
@@ -67,25 +60,9 @@ for i, source_prop in enumerate(source_props):
 		tencode = source_prop[source_tencode_field]
 		account_num = source_prop[source_account_num_field]
 		location = source_prop[source_address_field]
-		unit = source_prop[source_unit_field]
-		address_suffix = source_prop[source_address_suffix_field]
 
 		# Handle address
 		source_address = location.strip()
-		if unit:
-			unit = unit.lstrip('0')  # Remove leading zeros from unis
-			source_address = '{} #{}'.format(source_address, unit)
-
-		# Append address suffix (e.g. 101A)
-		if address_suffix and address_suffix.isalpha():
-			if not re.match('\d+[A-Z]', source_address):
-				# FEEDBACK: missing suffix
-				if '-' in source_address:
-					insert_i = source_address.index('-')
-				else:
-					insert_i = source_address.index(' ')
-				source_address = source_address[:insert_i] + address_suffix + \
-					source_address[insert_i:]
 
 		# Parse
 		try:
@@ -108,18 +85,13 @@ for i, source_prop in enumerate(source_props):
 			'tencode': tencode,
 			'owners': owners,
 			'address_low': comps['address']['low_num'],
-			# 'address_low_suffix': comps['address']['low_suffix'] or '',
 			'address_low_suffix': comps['address']['addr_suffix'] or '',
-			# 'address_low_frac': comps['address']['low_fractional'] or '',
-			# 'address_low_frac': comps['address']['fractional'] or '',
 			'address_high': comps['address']['high_num_full'],
 			'street_predir': comps['street']['predir'] or '',
 			'street_name': comps['street']['name'],
 			'street_suffix': comps['street']['suffix'] or '',
 			'street_postdir': comps['street']['postdir'] or '',
-			# 'unit_num': comps['unit']['num'] or '',
 			'unit_num': comps['address_unit']['unit_num'] or '',
-			# 'unit_type': comps['address_unit']['type'] or '',
 			'unit_type': comps['address_unit']['unit_type'] or '',
 			'street_address': street_address,
 		}
@@ -144,7 +116,5 @@ prop_table.create_index('street_address')
 FINISH
 '''
 
-# source_db.close()
-#ais_source_db.close()
 db.close()
 print('Finished in {} seconds'.format(datetime.now() - start))
