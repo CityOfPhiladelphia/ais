c3085d0528d108b031015a6d854f43a387f9bb57
floptical <stephen.macdavid@gmail.com>
2023-02-28 18:29:25 +0000

    export oracle envs cause they didnt appear to be getting here

--- ais/engine/bin/build_engine.sh
+++ ais/engine/bin/build_engine.sh
@@ -2,6 +2,10 @@
 
 WORKING_DIRECTORY=/home/ubuntu/ais
 
+export ORACLE_HOME=/usr/lib/oracle/18.5/client64
+export PATH=$PATH:$ORACLE_HOME/bin
+export LD_LIBRARY_PATH=$ORACLE_HOME/lib
+
 echo "Activating virtual environment"
 source $WORKING_DIRECTORY/venv/bin/activate
 # Add the ais folder with our __init__.py so we can import it as a python module
