d213cd7e57a1b9c059baaa75d18691cd63113eda
Alexander-M-Waldman <alex.waldman@phila.gov>
2023-08-17 17:32:12 -0400

    Add address error report

--- ais/engine/bin/make_reports.py
+++ ais/engine/bin/make_reports.py
@@ -22,6 +22,7 @@ address_summary_write_table_name = 'ADDRESS_SUMMARY'
 service_area_summary_write_table_name = 'SERVICE_AREA_SUMMARY'
 dor_condo_error_table_name = 'DOR_CONDOMINIUM_ERROR'
 true_range_write_table_name = 'TRUE_RANGE'
+address_error_write_table_name = 'AIS_ADDRESS_ERROR'
 read_conn = psycopg2.connect(
     "dbname={db_name} user={user}".format(db_name=parsed_read_db_string['db_name'], user=parsed_read_db_string['user']))
 #########################################################################################################################
@@ -166,6 +167,11 @@ def concatenate_dor_address(source_comps):
 #############################################
 # Read in files, format and write to tables #
 #############################################
+#################
+# ADDRESS ERROR #
+#################
+print("Writing address_error table...")
+etl.fromdb(read_conn, 'select * from address_error').rename('level', 'error_or_warning').tooraclesde(write_dsn, address_error_write_table_name)
 ##############
 # TRUE RANGE #
 ##############
