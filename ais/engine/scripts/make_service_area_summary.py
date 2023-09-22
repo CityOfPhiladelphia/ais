309eab950bf79342f770cc469ba01a784e159114
James Midkiff <james.midkiff@phila.gov>
2023-09-22 12:46:21 -0400

    Sort zoning_rcos in make_service_area_summary

--- ais/engine/scripts/make_service_area_summary.py
+++ ais/engine/scripts/make_service_area_summary.py
@@ -141,14 +141,17 @@ for i, address_summary_row in enumerate(address_summary_rows):
 		# Create and insert summary row
 		sa_summary_row = deepcopy(sa_summary_row_template)
 		sa_summary_row['street_address'] = street_address
-		update_dict = {}
+		zoning_rco = []
 		if sa_rows is not None:
-		    for x in sa_rows:
-		        if x['layer_id'] != 'zoning_rco':
-		            update_dict[x['layer_id']] = x['value']
-		        else:
-		            update_dict[x['layer_id']] = x['value'] if not x['layer_id'] in update_dict else update_dict[x['layer_id']] + '|' + x['value']
-		    sa_summary_row.update(update_dict)
+			for x in sa_rows:
+				if x['layer_id'] == 'zoning_rco':
+					zoning_rco.append(x['value'])
+				else:
+					sa_summary_row[x['layer_id']] = x['value']
+			zoning_rco.sort()
+			zoning_rcos = '|'.join(zoning_rco)
+			if zoning_rcos: 
+				sa_summary_row['zoning_rco'] = zoning_rcos
 
 		sa_summary_rows.append(sa_summary_row)
 
