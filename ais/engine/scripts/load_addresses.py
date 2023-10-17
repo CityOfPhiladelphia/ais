e52704cb87357136c73151b3dacb1818812c384a
Alexander-M-Waldman <alex.waldman@phila.gov>
2023-10-17 15:41:52 -0400

    Optimize spatial join using st_dwithin instead of st_intersects with st_buffer

--- ais/engine/scripts/load_addresses.py
+++ ais/engine/scripts/load_addresses.py
@@ -423,15 +423,16 @@ address_tag_strings = set()
 print("Adding dor_parcel_id tags for address points via spatial join...")
 dor_parcel_id_tag_stmt = '''
 insert into address_tag (street_address, key, value)
-select ng911.street_address, 'dor_parcel_id', dor.parcel_id 
+select ng911.street_address, 'dor_parcel_id' as key, dor.parcel_id as value 
 from ng911_address_point ng911 
-join dor_parcel dor on st_intersects(dor.geom, st_buffer(ng911.geom,2))
 join (
     select street_address from address_tag 
     except (
         select street_address from address_tag where key = 'dor_parcel_id'
         )
-    ) atag on atag.street_address = ng911.street_address'''
+    ) atag on atag.street_address = ng911.street_address
+join dor_parcel dor on st_dwithin(dor.geom, ng911.geom,2)
+'''
 db.execute(dor_parcel_id_tag_stmt)
 
 print("Adding pwd_parcel_id tags for address points via spatial join...")
@@ -439,13 +440,13 @@ pwd_parcel_id_tag_stmt = '''
 insert into address_tag (street_address, key, value)
 select ng911.street_address, 'pwd_parcel_id', pwd.parcel_id 
 from ng911_address_point ng911 
-join pwd_parcel pwd on st_intersects(pwd.geom, st_buffer(ng911.geom,2))
 join (
     select street_address from address_tag 
     except (
         select street_address from address_tag where key = 'pwd_parcel_id'
         )
     ) atag on atag.street_address = ng911.street_address
+join pwd_parcel pwd on st_dwithin(pwd.geom, ng911.geom,2)
 '''
 db.execute(pwd_parcel_id_tag_stmt)
 db.save()
