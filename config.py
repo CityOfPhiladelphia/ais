a856616978462eefad84f0ff390bcc47b45549a7
mbjackson-capp <mbjackson10@gmail.com>
2025-05-02 16:10:21 +0000

    fix to actual correct database

--- config.py
+++ config.py
@@ -384,8 +384,8 @@ ADDRESSES = {
         },
         {
             'name':                 'li_eclipse_location_ids',
-            'table':                'gis_lni.active_retired_parcels',
-            'db':                   'gislni',
+            'table':                'viewer_li.active_retired_parcels',
+            'db':                   'citygeo',
             'address_fields':       {
                 'base_address':       'base_address',
                 'unit_num':           'unit_num',
